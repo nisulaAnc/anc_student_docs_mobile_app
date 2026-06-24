4f9556e9f5647942eef3fa69aefae4f9abc5f32f
nisulaAnc <nisula@ancedu.com>
2026-06-24 13:40:18 +0530

    updated FileSystem import to legacy version for compatibility

--- frontend/src/components/UploadBox.jsx
+++ frontend/src/components/UploadBox.jsx
@@ -8,7 +8,7 @@ import * as DocumentPicker from 'expo-document-picker';
 import * as ImagePicker from 'expo-image-picker';
 import * as Print from 'expo-print';
 import * as Sharing from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { useTheme } from '../context/ThemeContext';
 
 const { width: SCREEN_W } = Dimensions.get('window');
