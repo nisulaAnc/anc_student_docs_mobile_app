e871b83d7997dfdc9ca58bff8af586fe89076270
nisulaAnc <nisula@ancedu.com>
2026-09-02 12:17:18 +0530

    added Staff Login, Registration, and Forgot Password screens to AppNavigator

--- frontend/src/navigation/AppNavigator.jsx
+++ frontend/src/navigation/AppNavigator.jsx
@@ -9,6 +9,9 @@ import CounsellorPortalScreen from '../screens/CounsellorPortalScreen';
 import StudentPortalScreen from '../screens/StudentPortalScreen';
 import QRScanScreen from '../screens/QRScanScreen';
 import LaunchScreen from '../screens/LaunchScreen';
+import StaffLoginScreen from '../screens/StaffLoginScreen';
+import StaffRegisterScreen from '../screens/StaffRegisterScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
 
 const Stack = createStackNavigator();
 
@@ -22,6 +25,9 @@ export default function AppNavigator() {
         <Stack.Screen name="CFDashboard" component={CFDashboardScreen} />
         <Stack.Screen name="CounsellorPortal" component={CounsellorPortalScreen} />
         <Stack.Screen name="StudentPortal" component={StudentPortalScreen} />
+        <Stack.Screen name="StaffLogin" component={StaffLoginScreen} />
+        <Stack.Screen name="StaffRegister" component={StaffRegisterScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="QRScan" component={QRScanScreen} />
       </Stack.Navigator>
     </NavigationContainer>
